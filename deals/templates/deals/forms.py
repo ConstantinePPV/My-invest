0598329260920fb189c8aaa4d62f03608cc85dee
ConstantinePPV <constantine.ppv@gmail.com>
2020-02-23 02:05:36 +0400

    Добавил возможность просматривать отдельные сделки + Обновил главный баннер

---
+++ deals/templates/deals/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import Deal
+
+
+class DealForm(forms.ModelForm):
+
+    class Meta:
+        model = Deal
+        fields = (
+            'date_deal', 
+            'stock', 
+            'price_deal', 
+            'volume_deal',
+            'sum_deal',
+        )
