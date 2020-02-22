897e1f3a3e3559d0330a2b27e6f6725622a2078d
ConstantinePPV <constantine.ppv@gmail.com>
2020-02-23 02:56:28 +0400

    Спать хоца! (

--- deals/urls.py
+++ deals/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.deal_table, name='deal_table'),
     path('deal/<int:pk>', views.deal_detail, name='deal_detail'),
+    path('deal/new/', views.deal_new, name='deal_new'),
 ]
--- deals/views.py
+++ deals/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Deal
+from .forms import DealForm
 
 
 def deal_table(request):
@@ -11,3 +12,8 @@ def deal_table(request):
 def deal_detail(request, pk):
     deal = get_object_or_404(Deal, pk=pk)
     return render(request, 'deals/deal_detail.html', {'deal': deal})
+
+
+def deal_new(request):
+    form = DealForm()
+    return render(request, 'deals/deal_edit.html', {'form': form})
