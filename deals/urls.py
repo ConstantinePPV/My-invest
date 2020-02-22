e830653bf9e29e23b2e01f037eca526028dfc3bb
ConstantinePPV <constantine.ppv@gmail.com>
2020-02-23 01:14:56 +0400

    Немного расширил приложение, добавив deal_detail

--- deals/urls.py
+++ deals/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.deal_table, name='deal_table'),
+    path('deal/<int:pk>', views.deal_detail, name='deal_detail'),
 ]
--- deals/views.py
+++ deals/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Deal
+
 
 def deal_table(request):
     deals = Deal.objects.all()
     return render(request, 'deals/deal_table.html', {'deals': deals})
+
+
+def deal_detail(request, pk):
+    deal = get_object_or_404(Deal, pk=pk)
+    return render(request, 'deals/deal_detail.html', {'deal': deal})
